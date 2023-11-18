42af0241058c650548bad42d8a4f839b305832d7
Marcus Pang <marcuspangyuyang@gmail.com>
2023-11-18 20:57:13 +0300

    feat: add fetch for event page

--- frontend/src/pages/events/[id]/index.tsx
+++ frontend/src/pages/events/[id]/index.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { useQuery } from "wagmi";
 
 export async function getServerSideProps() {
   return {
@@ -8,17 +9,41 @@ export async function getServerSideProps() {
   };
 }
 
+async function getEvent(id: number) {
+  try {
+    const data = await fetch(process.env.BACKEND_URL + "/events/" + id);
+    return data.json();
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
 export default function EventPage() {
   const { query } = useRouter();
   const id = query.id?.toString();
+  const { data } = useQuery([id, "events"], () => getEvent(+id!), {
+    enabled: Boolean(id),
+  });
 
   if (!id) {
     return null;
   }
 
+  if (!data) {
+    return (
+      <div>
+        <h1>No event found.</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="text-center">
-      <div className="p-10 mt-12 rounded-full border shadow mx-auto">LOGO</div>
+      <img
+        src={`https://gateway.lighthouse.storage/ipfs/${data.event.hash}`}
+        alt="Logo"
+      />
       <div className="flex justify-center pt-12">
         <w3m-connect-button />
         <w3m-network-button />
