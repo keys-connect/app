d5a286f747f49bc6dab38aa507b230a6d85390ea
pepo <pepo.ospina@gmail.com>
2023-11-18 21:24:44 +0100

    wip

--- contracts/tasks/request.mint.ts
+++ contracts/tasks/request.mint.ts
@@ -1,10 +1,13 @@
 import { task } from 'hardhat/config';
 
 task('request-mint', 'request a trusted minimized mint', async (taskArgs, hre) => {
-  const viem = (hre as any).viem;
+  const viem = hre.viem;
   const accounts = await viem.getWalletClients();
 
-  for (const account of accounts) {
-    console.log({ account });
-  }
+  const contractAddr = '0xB21ea083d9f17D5c9E47A99c1520F65368D8df62';
+  const subscriptionId = 825;
+  const slotId = 0;
+  const callbackGasLimit = 250_000;
+
+  console.log(process.env.GRAPH_KEY);
 });
