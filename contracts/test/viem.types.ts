883b92c2ef36e830921a1dc1641399c130948ffa
pepo <pepo.ospina@gmail.com>
2023-11-18 14:16:14 +0100

    first test passing

--- contracts/test/viem.types.ts
+++ contracts/test/viem.types.ts
@@ -1,6 +1,6 @@
 import { PublicClient, Account, Chain, Transport, WalletClient, decodeEventLog, GetContractReturnType } from 'viem';
 
-import { tokenFactoryABI, tokenABI, iVerifierABI } from '../export/abis';
+import { tokenFactoryABI, tokenABI, iVerifierABI, testErc20ABI } from '../export/abis';
 
 export type TransactionReceipt = Awaited<ReturnType<PublicClient['waitForTransactionReceipt']>>;
 export type Signer = WalletClient<Transport, Chain, Account>;
@@ -8,7 +8,7 @@ export type Signer = WalletClient<Transport, Chain, Account>;
 export type TokenFactory = GetContractReturnType<typeof tokenFactoryABI, PublicClient, Signer>;
 export type Token = GetContractReturnType<typeof tokenABI, PublicClient, Signer>;
 export type IVerifier = GetContractReturnType<typeof iVerifierABI, PublicClient, Signer>;
-
+export type TestErc20 = GetContractReturnType<typeof testErc20ABI, PublicClient, Signer>;
 
 export type EventType = Awaited<ReturnType<typeof decodeEventLog>>;
 export type TransferEventType = Awaited<ReturnType<typeof decodeEventLog<typeof tokenABI, 'Transfer'>>>;
