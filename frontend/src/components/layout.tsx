f33c5e3334af794b08973d5e76d4778535e28c06
Marcus Pang <marcuspangyuyang@gmail.com>
2023-11-18 23:36:44 +0300

    feat: add footer

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { NavBar } from "./navbar";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Footer } from "./footer";
 
 interface Props {
   children?: ReactNode;
@@ -17,6 +18,7 @@ export function Layout({ children, showNetworkButtons }: Props) {
     <div className={cn("min-w-screen min-h-screen h-full", fontSans.variable)}>
       <NavBar hideNetworkButtons={showNetworkButtons} />
       <div className="max-w-7xl mx-auto py-12">{children}</div>
+      <Footer />
     </div>
   );
 }
