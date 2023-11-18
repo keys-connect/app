4e613a20cac8ee2e7933eb31d24732a1ea27c023
pepo <pepo.ospina@gmail.com>
2023-11-18 21:29:45 +0100

    deployed verifier

--- contracts/ignition/modules/TokenFactory.ts
+++ contracts/ignition/modules/TokenFactory.ts
@@ -1,6 +1,7 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 
 export default buildModule('TokenFactory', (m) => {
+  const verifier = m.contract('ERC20BalanceOf', []);
   const master = m.contract('Token', []);
   const factory = m.contract('TokenFactory', [master]);
   return { factory };
