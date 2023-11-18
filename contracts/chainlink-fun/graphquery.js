d9710a304e0030415e81bdfea2d21fb7b89a3d80
pepo <pepo.ospina@gmail.com>
2023-11-18 21:44:37 +0100

    sim function

--- contracts/chainlink-fun/graphquery.js
+++ contracts/chainlink-fun/graphquery.js
@@ -1,10 +1,10 @@
-const graphKey = secrets.graphKey
+const graphKey = secrets.graphKey;
 
 const graphRequest = Functions.makeHttpRequest({
   url: `https://gateway-arbitrum.network.thegraph.com/api/${graphKey}/subgraphs/id/HUZDsRpEVP2AvzDCyzDHtdc64dyDxx8FQjzsmqSg4H3B`,
-  method: "POST",
+  method: 'POST',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 
   data: {
@@ -23,24 +23,24 @@ const graphRequest = Functions.makeHttpRequest({
                 }
             }`,
   },
-})
+});
 
-const [graphResponse] = await Promise.all([graphRequest])
+const [graphResponse] = await Promise.all([graphRequest]);
 
-let liquidities = []
+let liquidities = [];
 if (!graphResponse.error) {
   for (let i = 0; i < 3; i++) {
-    liquidities.push(graphResponse.data.data.poolDayDatas[i].liquidity)
+    liquidities.push(graphResponse.data.data.poolDayDatas[i].liquidity);
   }
 } else {
-  console.log("graphReponse Error, ", graphResponse)
+  console.log('graphReponse Error, ', graphResponse);
 }
 
 // check if liquidity is increasing
 // if it is increasing, return 1 and functionsConsumer trigger the function to swap WMATIC to WETH.
 // if it is not, return 0 and functionsConsumer does nothing.
 if (liquidities[0] > liquidities[1] && liquidities[0] > liquidities[2]) {
-  console.log("liquidity is increasing")
-  return Functions.encodeUint256(0)
+  console.log('liquidity is increasing');
+  return Functions.encodeUint256(0);
 }
-return Functions.encodeUint256(1)
+return Functions.encodeUint256(1);
