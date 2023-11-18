7f106add020a1438b802c6ef487b064d2306526e
Marcus Pang <marcuspangyuyang@gmail.com>
2023-11-18 23:31:09 +0300

    feat: finalise the landing page

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -16,7 +16,7 @@ export function Layout({ children, showNetworkButtons }: Props) {
   return (
     <div className={cn("min-w-screen min-h-screen h-full", fontSans.variable)}>
       <NavBar hideNetworkButtons={showNetworkButtons} />
-      <div className="max-w-7xl mx-auto my-12">{children}</div>
+      <div className="max-w-7xl mx-auto py-12">{children}</div>
     </div>
   );
 }
