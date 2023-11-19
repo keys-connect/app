c4a34a75b2645ec64523dad44d3175f95cd3dbbe
Marcus Pang <marcuspangyuyang@gmail.com>
2023-11-19 08:40:09 +0300

    fix: events page

--- frontend/src/pages/events/index.tsx
+++ frontend/src/pages/events/index.tsx
@@ -3,7 +3,9 @@ import { useQuery } from "wagmi";
 
 async function getEvents() {
   try {
-    const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + "/events");
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_BACKEND_URL + "/events"
+    );
     const json = await response.json();
     return json;
   } catch (e) {
@@ -21,7 +23,7 @@ export default function EventsPage() {
       </div>
     );
   }
-
+  console.log(data);
   return (
     <section>
       <h1 className="text-2xl font-bold tracking-tight">Events Page</h1>
@@ -29,24 +31,24 @@ export default function EventsPage() {
         {data.events.map((event: any) => (
           <div className="rounded-lg overflow-hidden shadow-md" key={event.id}>
             <img
-              alt="Event 1"
-              className="object-cover w-full h-60"
-              height="300"
-              src="/placeholder.svg"
+              src={`https://gateway.lighthouse.storage/ipfs/${event.hash}`}
+              alt="Logo"
               style={{
                 aspectRatio: "400/300",
                 objectFit: "cover",
               }}
+              height="300"
               width="400"
+              className="w-full h-60 object-cover"
             />
             <div className="p-6 bg-white dark:bg-zinc-950">
-              <h3 className="text-lg font-semibold">Event Title 1</h3>
+              <h3 className="text-lg font-semibold">{event.title}</h3>
               <p className="mt-2 text-zinc-500 dark:text-zinc-400">
-                Event description goes here.
+                {event.description}
               </p>
               <Link
                 className="mt-4 inline-flex text-primary-500 hover:text-primary-600 dark:text-primary-400 dark:hover:text-primary-500"
-                href="#"
+                href={`/events/${event.id}`}
               >
                 Learn More
               </Link>
